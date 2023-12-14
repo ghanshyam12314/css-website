98cf9cdf1c3dd7b40d8e4b4834523edfcdb92cda
ghanshyam12314 <153067875+ghanshyam12314@users.noreply.github.com>
2023-12-14 14:05:36 +0530

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,38 @@
+$('#banner_part').owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:2000,
+
+    navText:['<i class="fa-solid fa-arrow-left"></i>','<i class="fa-solid fa-arrow-right"></i>'],
+})
+
+$('#slider1').owlCarousel({
+    loop:true,
+    margin:30,
+    // nav:true,
+    items:4,
+    autoplay:true,
+    autoplayTimeout:2000,
+})
+$('#slider2').owlCarousel({
+    loop:true,
+    margin:20,
+    // nav:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+ 
+})
+$('#slider3').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    items:1,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    navText:['<i class="fa-solid fa-arrow-left"></i>','<i class="fa-solid fa-arrow-right"></i>'],
+})
